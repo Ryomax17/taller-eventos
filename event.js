aaba1e2da16a69a47862d2c32d6693b7ce4a4356
Diego Caceres <wadexrubyy@gmail.com>
2023-11-25 11:10:58 -0300

    solucion-click

--- event.js
+++ event.js
@@ -1,6 +1,12 @@
 var buttonDiv = document.getElementById("buttonDiv");
+var holaButton = document.getElementById("holaButton");
 
-buttonDiv.addEventListener("click", function() {
+holaButton.addEventListener("click", function(event) {
+    alert("Hola!");
+    event.stopPropagation();
+});
+
+buttonDiv.addEventListener("click", function(event) {
     alert("Hola! Soy el div");
     event.stopPropagation();
 });
